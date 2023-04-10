f1d7a93de882ff4a0de0f455494ef2fa38cf2e87
apkrishna79 <123661479+apkrishna79@users.noreply.github.com>
2023-04-09 23:18:00 -0500

    Add files via upload

---
+++ practice4.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+	<?php
+    if (isset($_POST["size"])) {
+        $size = intval($_POST["size"]);
+		if ($size > 0) {
+			echo "<table border='1'>";
+			echo "<tr><th></th>";
+
+			for ($i = 1; $i <= $size; $i++) {
+				echo "<th>" . $i . "</th>";
+			}
+			echo "</tr>";
+
+			for ($i = 1; $i <= $size; $i++) {
+				echo "<tr><th>" . $i . "</th>";
+				for ($j = 1; $j <= $size; $j++) {
+					echo "<td>" . ($i * $j) . "</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "<p>Enter a valid number.</p>";
+		}
+	}
+	?>
+
+</body>
+
+</html>
